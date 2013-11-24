01458154b7b4997e7f672eb972d9624d22f06f86
angie radtke <a.radtke@derauftritt.de>
2013-11-24 12:00:15 +0100

    clean up

--- administrator/templates/adminbeez/index.php
+++ administrator/templates/adminbeez/index.php
@@ -23,7 +23,7 @@ $doc->addScript('templates/' .$this->template. '/js/template.js');
 
 
 // Add Stylesheets
-$doc->addStyleSheet('templates/' . $this->template . '/css/uikit.css');
+$doc->addStyleSheet('templates/' . $this->template . '/css/template.css');
 
 // Load optional RTL Bootstrap CSS
 JHtml::_('bootstrap.loadCss', false, $this->direction);
